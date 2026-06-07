917d4497e00e3d1c29f3469a8d0c6e98bebcc2d3
İspik <ispik@ispik.dev>
2026-06-07 15:53:30 +0300

    feat: add coverage pages
    Signed-off-by: İspik <ispik@ispik.dev>

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     projects: ["./packages/*"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      include: ["packages/*/src/**/*.ts"],
+      exclude: ["packages/*/src/**/index.ts"],
+    }
   },
 });
